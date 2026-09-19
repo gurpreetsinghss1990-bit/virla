c5b05c2ea63bd34be9e99a5109891600179a7fe9
Virral Patel <gurpreetsinghss1990@gmail.com>
2026-09-19 14:34:23 +0530

    clean: remove duplicate upi queries and retain google maps configuration

--- plugins/withAndroidUpiAndMaps.js
+++ plugins/withAndroidUpiAndMaps.js
@@ -1,93 +1,10 @@
 const { withAndroidManifest } = require('@expo/config-plugins');
 
-const UPI_PACKAGES = [
-  'com.google.android.apps.nbu.paisa.user',
-  'com.phonepe.app',
-  'net.one97.paytm',
-  'in.org.npci.upiapp',
-  'in.amazon.mShop.android.shopping',
-  'com.mobikwik_new',
-  'com.freecharge.android',
-  'club.cred',
-  'com.myairtelapp',
-  'com.hcl.payzapp',
-  'com.icicibank.pockets',
-  'com.csam.icici.bank.imobile',
-  'com.sbi.upi',
-  'com.axisbank.upi',
-  'com.mipay.wallet.in',
-  'com.navi.app',
-  'com.ril.customerapp',
-  'com.tatanew.app',
-  'com.whatsapp',
-  'com.whatsapp.w4b',
-  'org.telegram.messenger',
-  'com.truecaller',
-];
-
 module.exports = function withAndroidUpiAndMaps(config) {
   return withAndroidManifest(config, async (config) => {
     const androidManifest = config.modResults.manifest;
 
-    // 1. Add QUERY_INTENT_PACKAGES permission
-    if (!androidManifest['uses-permission']) {
-      androidManifest['uses-permission'] = [];
-    }
-    const hasQueryPermission = androidManifest['uses-permission'].some(
-      (perm) => perm.$?.['android:name'] === 'android.permission.QUERY_INTENT_PACKAGES'
-    );
-    if (!hasQueryPermission) {
-      androidManifest['uses-permission'].push({
-        $: { 'android:name': 'android.permission.QUERY_INTENT_PACKAGES' },
-      });
-    }
-
-    // 2. Add queries section for upi intent and specific UPI packages
-    if (!androidManifest.queries) {
-      androidManifest.queries = [];
-    }
-
-    let queriesObj = androidManifest.queries[0];
-    if (!queriesObj) {
-      queriesObj = { intent: [], package: [] };
-      androidManifest.queries.push(queriesObj);
-    }
-    if (!queriesObj.intent) queriesObj.intent = [];
-    if (!queriesObj.package) queriesObj.package = [];
-
-    // upi scheme intent
-    const hasUpiIntent = queriesObj.intent.some(
-      (it) => it.data?.some((d) => d.$?.['android:scheme'] === 'upi')
-    );
-    if (!hasUpiIntent) {
-      queriesObj.intent.push({
-        action: [{ $: { 'android:name': 'android.intent.action.VIEW' } }],
-        data: [{ $: { 'android:scheme': 'upi' } }],
-      });
-    }
-
-    // upi://pay intent with host
-    const hasUpiPayIntent = queriesObj.intent.some(
-      (it) => it.data?.some((d) => d.$?.['android:scheme'] === 'upi' && d.$?.['android:host'] === 'pay')
-    );
-    if (!hasUpiPayIntent) {
-      queriesObj.intent.push({
-        action: [{ $: { 'android:name': 'android.intent.action.VIEW' } }],
-        data: [{ $: { 'android:scheme': 'upi', 'android:host': 'pay' } }],
-      });
-    }
-
-    // Add all UPI packages
-    for (const pkg of UPI_PACKAGES) {
-      const exists = queriesObj.package.some((p) => p.$?.['android:name'] === pkg);
-      if (!exists) {
-        queriesObj.package.push({
-          $: { 'android:name': pkg },
-        });
-      }
-    }
-
-    // 3. Ensure Google Maps API key meta-data exists in application
+    // Ensure Google Maps API key meta-data exists in application
     const app = androidManifest.application?.[0];
     if (app) {
       if (!app['meta-data']) {
@@ -109,3 +26,4 @@ module.exports = function withAndroidUpiAndMaps(config) {
     return config;
   });
 };
+
